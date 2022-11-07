6de5bd2729e23d3ffd3b5e403777ad313cc27853
TheMVProgrammer <franklin0415@hotmail.com>
2022-11-07 19:05:16 -0400

    Adding players and background
    Things added:
    
    Two players
    The sky
    The ground
    The moon
    Some animations
    Rigid body and box collider components to some objects.

---
+++ monster-chase/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveForce = 10f; // The variable that we are going to use to move the player.
+
+    public float jumpForce = 11f;
+
+    private float movementX;
+
+    private Rigidbody rb;
+
+    private SpriteRenderer sr;
+
+    private Animator animator;
+
+    private string WALK_ANIMATION = "Walk";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
